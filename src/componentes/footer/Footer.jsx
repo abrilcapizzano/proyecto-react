017324c626b7b00b07ea36c642ccc962d20a4a0e
abrilcapizzano <103966017+abrilcapizzano@users.noreply.github.com>
2022-11-27 18:55:18 -0300

    modis

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -4,7 +4,7 @@ import icon from "./react-1.svg"
 const Footer = () => {
     return (
      <footer className="footer">
-        <p className="tituloFooter">Ejemplo de app en React Native</p>
+        <p className="tituloFooter">React Native App</p>
 <img className="icon" src={icon} alt="icon-react" />
         <p>Todos los derechos de autor reservados</p>
         <p>Abril Capizzano</p>
